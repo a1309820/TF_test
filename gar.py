966b2cfeba69ac39cabbed256bdf46e5813090eb
Andrew Storey <andrewgstorey@gmail.com>
2017-02-01 21:50:32 -0500

    updated

--- gar.py
+++ gar.py
@@ -18,6 +18,16 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
+for i in range(1000):
+  batch = mnist.train.next_batch(100)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
