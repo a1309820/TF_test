f2ba539c79fab2c28f2ca0469181ccae061bf314
Andrew Storey <andrewgstorey@gmail.com>
2017-02-05 11:20:32 -0500

    Completed Quickstart iris model
    Also added a gitignore file to ignore example/quickstart/iris_model

--- myMnist_softmax_1.py
+++ myMnist_softmax_1.py
@@ -86,9 +86,9 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
-for i in range(2001):
+for i in range(10000):
   batch = mnist.train.next_batch(50)
-  if i%100 == 0:
+  if i%50 == 0:
     train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g"%(i, train_accuracy))
